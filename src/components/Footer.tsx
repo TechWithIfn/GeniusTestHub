4672ace0ed8af7b340bfe5b7b253f8bac1a5b38d
Irfan Ansari <Techwithifn@gmail.com>
2025-02-22 11:36:00 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,10 +33,10 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-semibold mb-4">Connect With Us</h3>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a href="https://github.com/TechWithIfn" className="text-gray-400 hover:text-white">
                 <Github className="h-6 w-6" />
               </a>
-              <a href="#" className="text-gray-400 hover:text-white">
+              <a href="https://www.linkedin.com/in/irfan-ansari04m/" className="text-gray-400 hover:text-white">
                 <Linkedin className="h-6 w-6" />
               </a>
               <a href="#" className="text-gray-400 hover:text-white">
